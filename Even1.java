d4a82f15f7afd821e60bafdb392bf838f830eeb7
rojinatiwari <rojinatiwari17@gmail.com>
2024-11-21 23:01:41 +0545

    even numbers from 1 to 100 using for loop

---
+++ Even1.java
@@ -0,0 +1,20 @@
+
+
+class Even1
+ {
+    public static void main (String [] args)
+    {
+        int n = 100;
+        System.out.println("Even numbers from 1 to 100:\n");
+        for(int i = 1; i<=n;i++)
+        {
+            if (i%2 == 0)
+            {
+                System.out.println(i + "");
+            }
+                
+            
+        }
+    }
+    
+}
