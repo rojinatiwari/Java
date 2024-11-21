139ee7931bc4b03bc0c2e8030ae5e57f776cedde
rojinatiwari <rojinatiwari17@gmail.com>
2024-11-21 23:23:58 +0545

    Even numbers from 1 to 100 using while loop

---
+++ Even2.java
@@ -0,0 +1,14 @@
+class Even2 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 100) {
+            if ((n % 2) == 0) {
+                System.out.println(n + "");
+            }
+            n++;
+
+        }
+
+    }
+
+}
