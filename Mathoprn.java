516da1e8cfa26f6a6cb8e63f6b39d81da219d2fd
rojinatiwari <rojinatiwari17@gmail.com>
2024-12-01 23:10:38 +0545

    accessing package using import keyword

---
+++ Mathoprn.java
@@ -0,0 +1,12 @@
+
+import operation.Arthoprn;
+
+public class Mathoprn {
+    public static void main(String[]args)
+    {
+        Arthoprn a = new Arthoprn();
+        System.out.println(a.add(4,5));
+        System.out.println(a.sub(4,5));
+    }
+    
+}
