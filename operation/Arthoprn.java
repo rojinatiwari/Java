ee9570ef61516e4d6bfe4c5e84c9461eadbb0ce0
rojinatiwari <rojinatiwari17@gmail.com>
2024-12-01 23:08:22 +0545

    custom package in java

---
+++ operation/Arthoprn.java
@@ -0,0 +1,12 @@
+package operation;
+public class Arthoprn {
+    public double add(double a , double b)
+    {
+        return (a+b);
+    }
+    public double sub(double a , double b)
+    {
+        return (a-b);
+    }
+    
+}
